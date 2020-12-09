816528049e3d0422dc84d00804745f45ca6b37b4
Banapple01 <48165909+Banapple01@users.noreply.github.com>
2020-12-08 21:13:53 -0700

    updated the create a room page
    allowing the page to communicate with the backend and create a new room object

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -10,60 +10,100 @@ import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
-const CreateRoomPage = (props) => {
-    var defaultVotes = 2;
-    return (
-        <Grid container spacing={1}>
-            <Grid item xs={12} align="center">
-                <Typography component="h4" variant="h4">
-                    Create A Room
-                </Typography>
-            </Grid>
-            <Grid item xs={12} align="center">
-                <FormControl component="fieldset">
-                    <FormHelperText>
-                        <div align="center">Guest Control of Playback State</div>
-                    </FormHelperText>
-                    <RadioGroup row defaultValue="true">
-                        <FormControlLabel 
-                            value="true"
-                            control={<Radio color="primary" />}
-                            label="Play/Pause"
-                            labelPlacement="bottom"
-                        />
-                        <FormControlLabel 
-                            value="false"
-                            control={<Radio color="secondary" />}
-                            label="No control"
-                            labelPlacement="bottom"
-                        />
-                    </RadioGroup>
-                </FormControl>
-            </Grid>
-            <Grid item xs={12} align="center">
-                <FormControl>
-                    <TextField 
-                    required={true} 
-                    type="number" 
-                    defaultValue={this.defaultVotes} 
-                    inputProps={{
-                        min: 1,
-                        style: {textAlign: "center"},
-                    }}
-                    />
-                    <FormHelperText>
-                        <div align="center">Votes Required To Skip Song</div>
-                    </FormHelperText>
-                </FormControl>
-            </Grid>
-            <Grid item xs={12} align="center">
-                <Button color="primary" variant="contained" >Create A Room</Button>
-            </Grid>
-            <Grid item xs={12} align="center">
-                <Button color="secondary" variant="contained" to="/" component={Link} >Back</Button>
-            </Grid>
-        </Grid>
-    )
-}
+export default class CreateRoomPage extends Component{
+    defaultVotes = 2;
 
-export default CreateRoomPage
+    constructor(props) {
+        super(props);
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes,
+        };
+
+        this.handelRoomButtonPressed = this.handelRoomButtonPressed.bind(this);
+        this.handelVotesChange = this.handelVotesChange.bind(this);
+        this.handelGuestCanPauseChange = this.handelGuestCanPauseChange.bind(this);
+    }
+
+    handelVotesChange(e) {
+        this.setState({
+            votesToSkip: e.target.value,
+        });
+    }
+
+    handelGuestCanPauseChange(e) {
+        this.setState({
+            guestCanPause: e.target.value === 'true' ? true : false,
+        });
+    }
+
+        handelRoomButtonPressed() {
+            const requestOptions = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    votes_to_skip: this.state.votesToSkip,
+                    guest_can_pause: this.state.guestCanPause,
+                }),
+            };
+            fetch("/api/create-room", requestOptions)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+        }
+
+    render() {
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography component="h4" variant="h4">
+                        Create A Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <FormControl component="fieldset">
+                        <FormHelperText>
+                            <div align="center">Guest Control of Playback State</div>
+                        </FormHelperText>
+                        <RadioGroup row defaultValue="true" onChange={this.handelGuestCanPauseChange}>
+                            <FormControlLabel 
+                                value="true"
+                                control={<Radio color="primary" />}
+                                label="Play/Pause"
+                                labelPlacement="bottom"
+                            />
+                            <FormControlLabel 
+                                value="false"
+                                control={<Radio color="secondary" />}
+                                label="No control"
+                                labelPlacement="bottom"
+                            />
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <FormControl>
+                        <TextField 
+                        required={true} 
+                        type="number" 
+                        onChange={this.handelVotesChange}
+                        defaultValue={this.defaultVotes} 
+                        inputProps={{
+                            min: 1,
+                            style: {textAlign: "center"},
+                        }}
+                        />
+                        <FormHelperText>
+                            <div align="center">Votes Required To Skip Song</div>
+                        </FormHelperText>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="primary" variant="contained" onClick={this.handelRoomButtonPressed}>Create A Room</Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="secondary" variant="contained" to="/" component={Link} >Back</Button>
+                </Grid>
+            </Grid>
+        );
+    }
+}
