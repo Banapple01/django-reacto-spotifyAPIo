4f40b5d30cb68d8712d597779ededbd97d8e1011
Banapple01 <48165909+Banapple01@users.noreply.github.com>
2020-12-28 18:04:09 -0700

    Got through tutorial 7
    running into a few errors with creating a room

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
 
 
 const HomePage = (props) => {
@@ -19,6 +20,7 @@ const HomePage = (props) => {
                 </Route>
                 <Route path="/join" component={RoomJoinPage} />
                 <Route path="/create" component={CreateRoomPage} />
+                <Route path="/room:roomCode" component={Room} />
             </Switch>
         </Router>
     )
--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Grid, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 
 const RoomJoinPage = (props) => {
+    // this.state or useState
+
     return (
-        <div>
-            <p>This is the RoomJoinPage</p>
-        </div>
+        <Grid container spacing={1} alignItems="center">
+            <Grid item xs={12}>
+                <Typography variant="h4" component="h4">
+                    Join a Room
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                error="error"
+                label="Code"
+                placeholder="Enter a Room Code"
+                value={}
+                helperText={}
+                variant="outlined"
+                />
+            </Grid>
+        </Grid>
     )
 }
 
